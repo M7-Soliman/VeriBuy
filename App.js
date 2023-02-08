e83c30263711cd8d8747c555d2eadb5a1cd1fe44
Mohamed (Soliman) Elhussiny <mohamed.s.elhussiny@gmail.com>
2023-02-09 00:59:10 +0200

    Test 2

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 import HomeScreen from './screens/HomeScreen';
-//ssdsdsds
+//Ahmed Hesham
 
 const Stack = createNativeStackNavigator();
 
