555cd1c9a2b28368cf45bcf6e46d0d114764b471
Mohamed (Soliman) Elhussiny <mohamed.s.elhussiny@gmail.com>
2023-02-08 04:23:30 +0200

    Added initial Home Screen Components

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,8 +1,20 @@
-import { View, Text, SafeAreaView } from 'react-native'
+import { View, Text, SafeAreaView, TextInput, ScrollView } from 'react-native'
 import React, { useLayoutEffect } from 'react'
 import { useNavigation } from '@react-navigation/native'
-import { searchIcon } from "react-native-heroicons/outline";
-import { TextInput } from 'react-native-web';
+
+
+
+import { UserIcon as Uout} from "react-native-heroicons/outline";
+import { UserIcon as Usol} from "react-native-heroicons/solid";
+
+import { ShoppingCartIcon as Cartout} from "react-native-heroicons/outline";
+import { ShoppingCartIcon as Cartsol} from "react-native-heroicons/solid";
+
+import { MagnifyingGlassIcon  as Searchout} from "react-native-heroicons/outline";
+import { MagnifyingGlassIcon as Searchsol} from "react-native-heroicons/solid";
+
+// import { searchIcon } from "react-native-heroicons/outline";
+// import { TextInput } from 'react-native-web';
 
 const HomeScreen = () => {
     const navigation = useNavigation();
@@ -16,9 +28,30 @@ const HomeScreen = () => {
 
   return (
     <SafeAreaView className="bg-white pt 6">
-            {/* search  */}
         <View>
-            
+
+          {/* Header */}
+          <View className = "flex-row pb-3 items-center mx-4 space-x-5">
+  
+            <View className="flex-row flex-1 space-x-2">
+              <Searchout size = {32} color = "#39c58f"/>
+              <TextInput placeholder="Search all products" keyboardType="Default"/>
+            </View>
+              
+            <View className="flex-row space-x-6">
+              <Cartout size = {32} color = "#39c58f"></Cartout>
+              <Uout size = {32} color = "#39c58f"></Uout>
+            </View>
+
+          </View>
+
+          {/* Main */}
+          <ScrollView> 
+
+
+
+          </ScrollView>
+
         </View> 
     </SafeAreaView>
   )
