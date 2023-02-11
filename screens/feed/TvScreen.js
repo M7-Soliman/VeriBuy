9ee69ca997aa1a6da91bd34fc1a3b7a2218015b1
aiiitsh <ahesham368@gmail.com>
2023-02-11 21:09:37 +0200

    TvScreen screen functionality

--- screens/feed/TvScreen.js
+++ screens/feed/TvScreen.js
@@ -1,12 +1,26 @@
-import { View, Text } from 'react-native'
 import React from 'react'
-
-const TvScreen = () => {
+import { View, Text, FlatList } from 'react-native'
+import styles from './styles'
+import { Dimensions } from 'react-native'
+export default function TvScreen () {
+  const array = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const renderItem = ({item, index})=>{
+    return (
+       <View style={[{ flex: 1, height: Dimensions.get('window').height -56 },index % 2 == 0 ? {backgroundColor: 'blue'} : {backgroundColor: 'black'}]}>
+          <Text>{item}</Text> 
+       </View>
+    )
+    
+  }
   return (
-    <View>
-      <Text>Videos</Text>
+    <View style={styles.container}>
+      <FlatList
+      data={array}
+      renderItem={renderItem}
+      pagingEnabled
+      keyExtractor={item => item}
+      decelerationRate={'fast'}
+      />
     </View>
   )
 }
-
-export default TvScreen
