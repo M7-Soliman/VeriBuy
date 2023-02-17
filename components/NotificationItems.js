725e83ab01d08dbe5068106d4ea79b114d8d52ba
Mohamed (Soliman) Elhussiny <mohamed.s.elhussiny@gmail.com>
2023-02-17 03:14:18 +0200

    Committing before firebase config

--- components/NotificationItems.js
+++ components/NotificationItems.js
@@ -10,81 +10,59 @@ const NotificationItems = () => {
     const navigation = useNavigation();
   return (
     <ScrollView contentContainerStyle = {{ paddingTop:10, paddingLeft: 15, paddingRight: 15, }} showsHorizontalScrollIndicator={false}>
-        <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1jTKPmVRijC79z_Dj3ER5VE6HAdkVHfpT" 
+        <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1oKPDzeyfda3pOQ00UcwJzM_q8LeB0drL" 
       title = "Shosho Gnan"
-      body = "H araf mayteny faash5 w ksom cringe circle" 
-      button_text = "Press and u r a cunt"
-      button_navigate = ""
+      body = "Abo cringe circle" 
       specificurl = ""/>
     
-        <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1jTKPmVRijC79z_Dj3ER5VE6HAdkVHfpT" 
+        <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1oKPDzeyfda3pOQ00UcwJzM_q8LeB0drL" 
       title = "Shosho Gnan"
-      body = "H araf mayteny faash5 w ksom cringe circle" 
-      button_text = "Press and u r a cunt"
-      button_navigate = ""
+      body = "Abo cringe circle" 
       specificurl = ""/>
       
-        <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1jTKPmVRijC79z_Dj3ER5VE6HAdkVHfpT" 
+        <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1oKPDzeyfda3pOQ00UcwJzM_q8LeB0drL" 
       title = "Shosho Gnan"
-      body = "H araf mayteny faash5 w ksom cringe circle" 
-      button_text = "Press and u r a cunt"
-      button_navigate = ""
+      body = "Abo cringe circle" 
       specificurl = ""/>
 
-        <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1jTKPmVRijC79z_Dj3ER5VE6HAdkVHfpT" 
+        <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1oKPDzeyfda3pOQ00UcwJzM_q8LeB0drL" 
       title = "Shosho Gnan"
-      body = "H araf mayteny faash5 w ksom cringe circle" 
-      button_text = "Press and u r a cunt"
-      button_navigate = ""
+      body = "Abo cringe circle" 
       specificurl = ""/>
 
-        <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1jTKPmVRijC79z_Dj3ER5VE6HAdkVHfpT" 
+        <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1oKPDzeyfda3pOQ00UcwJzM_q8LeB0drL" 
       title = "Shosho Gnan"
-      body = "H araf mayteny faash5 w ksom cringe circle" 
-      button_text = "Press and u r a cunt"
-      button_navigate = ""
+      body = "Abo cringe circle" 
       specificurl = ""/>
 
-            <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1jTKPmVRijC79z_Dj3ER5VE6HAdkVHfpT" 
+            <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1oKPDzeyfda3pOQ00UcwJzM_q8LeB0drL" 
       title = "Shosho Gnan"
-      body = "H araf mayteny faash5 w ksom cringe circle" 
-      button_text = "Press and u r a cunt"
-      button_navigate = ""
+      body = "Abo cringe circle" 
       specificurl = ""/>
 
-            <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1jTKPmVRijC79z_Dj3ER5VE6HAdkVHfpT" 
+            <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1oKPDzeyfda3pOQ00UcwJzM_q8LeB0drL" 
       title = "Shosho Gnan"
-      body = "H araf mayteny faash5 w ksom cringe circle" 
-      button_text = "Press and u r a cunt"
-      button_navigate = ""
+      body = "Abo cringe circle" 
       specificurl = ""/>
 
-            <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1jTKPmVRijC79z_Dj3ER5VE6HAdkVHfpT" 
+            <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1oKPDzeyfda3pOQ00UcwJzM_q8LeB0drL" 
       title = "Shosho Gnan"
-      body = "H araf mayteny faash5 w ksom cringe circle" 
-      button_text = "Press and u r a cunt"
-      button_navigate = ""
+      body = "Abo cringe circle" 
       specificurl = ""/>
 
-        <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1jTKPmVRijC79z_Dj3ER5VE6HAdkVHfpT" 
+        <SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1oKPDzeyfda3pOQ00UcwJzM_q8LeB0drL" 
       title = "Shosho Gnan"
-      body = "H araf mayteny faash5 w ksom cringe circle" 
-      button_text = "Press and u r a cunt"
-      button_navigate = ""
+      body = "Abo cringe circle" 
       specificurl = ""/>
 
-<SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1jTKPmVRijC79z_Dj3ER5VE6HAdkVHfpT" 
+<SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1oKPDzeyfda3pOQ00UcwJzM_q8LeB0drL" 
       title = "Shosho Gnan"
-      body = "H araf mayteny faash5 w ksom cringe circle" 
-      button_text = "Press and u r a cunt"
-      button_navigate = ""
+      body = "Abo cringe circle" 
       specificurl = ""/>
 
-<SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1jTKPmVRijC79z_Dj3ER5VE6HAdkVHfpT" 
+<SubNotifications url1 = "https://drive.google.com/uc?export=view&id=1oKPDzeyfda3pOQ00UcwJzM_q8LeB0drL" 
       title = "Shosho Gnan"
-      body = "H araf mayteny faash5 w ksom cringe circle" 
-      button_text = "Press and u r a cunt"
-      button_navigate = ""
+      body = "Abo cringe circle" 
       specificurl = ""/>
 
 
--- components/SubNotifications.js
+++ components/SubNotifications.js
@@ -1,4 +1,4 @@
-import { View, Text, ScrollView, TouchableOpacity, Image, StyleSheet} from 'react-native'
+import { View, Text, ScrollView, TouchableOpacity, Image, StyleSheet, ImageBackgroundBase} from 'react-native'
 import React from 'react'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 
@@ -9,22 +9,28 @@ import { useNavigation } from '@react-navigation/native'
 
 import { ShoppingCartIcon as Cartout} from "react-native-heroicons/outline";
 import { ShoppingCartIcon as Cartsol} from "react-native-heroicons/solid";
+import { platformColor } from 'nativewind';
 
 const SubNotifications = ({url1, url2}) => {
   
   return (
-    <View style={{height: 90, width: "100%"}} className = "bg- bg-gray-200 opacity-90 pt-5 flex-row pb-10 items-center mt-3 rounded-3xl" showsHorizontalScrollIndicator={false}>
+    <View style={{height: 90, width: "100%"}} className = "bg-gray-200 pt-5 flex-row pb-10 items-center mt-3 rounded-3xl" showsHorizontalScrollIndicator={false}>
           
             
-            <View style = {styles.thathellofabutton} className="flex-row">
+            <View style = {styles.profilepic} className="flex-row">
             <Image source={{uri: url1}}
             style={{width: wp('15') , height: wp('15')} } className="rounded-3xl"/>
             </View>
-            
-            <View style = {styles.thathellofabutton} className="flex-row">
-            <Image source={{uri: url1}}
-            style={{width: wp('15') , height: wp('15')} } className="rounded-3xl"/>
+{/*             
+            <View style = {styles.buttonright} className="flex-row">
+            <View style={{width: wp('27') , height: wp('12'), backgroundColor:"#26da76"}} className="rounded-3xl"/>
             </View>
+             */}
+            <View style = {styles.title} className="flex-row">
+            <Text style={{width: wp('27') , height: wp('12'), fontSize:18, fontWeight: 'bold'}} className="Bold">Title</Text>
+            </View>
+
+
 
 
     </View>
@@ -32,10 +38,23 @@ const SubNotifications = ({url1, url2}) => {
 }
 const styles = StyleSheet.create({
 
-  thathellofabutton: {
+  profilepic: {
     position: "relative",
     bottom: wp('-2.5'),
-    left: wp('4.5')
-  }})
+    left: wp('4')
+  },
+  title: {
+    position: "relative",
+    bottom: wp('-3'),
+    left: wp('-15'), 
+    fontSize: 50,
+    color:'red' },
+    
+    buttonright: {
+    position: "relative",
+    bottom: wp('-2.5'),
+    left: wp('45')}, 
+
+})
 
 export default SubNotifications
