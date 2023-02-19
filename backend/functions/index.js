6444685df01f18b5a3e73240d4a7944b3c21965f
Mohamed (Soliman) Elhussiny <mohamed.s.elhussiny@gmail.com>
2023-02-19 11:59:45 +0200

    Adding user to database

--- backend/functions/index.js
+++ backend/functions/index.js
@@ -1,5 +1,19 @@
 const functions = require("firebase-functions");
 
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+const db = admin.firestore()
+
+exports.newUser = functions.auth.user().onCreate((user) => {
+    return db
+        .collection("user")
+        .doc(user.uid)
+        .create(JSON.parse(JSON.stringify(user)))
+})
+
+
 // // Create and deploy your first functions
 // // https://firebase.google.com/docs/functions/get-started
 //
