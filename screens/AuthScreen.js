5e8ba720cc1d047b1b2172e1aef44782fcfc881d
Mohamed (Soliman) Elhussiny <mohamed.s.elhussiny@gmail.com>
2023-02-19 02:03:19 +0200

    Fixing design to work across all devices

--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -49,27 +49,21 @@ const AuthScreen = () => {
 const styles = StyleSheet.create({
 
     vbuylogo:{
-        bottom: wp('-37'),
-        left: wp('50')-(wp('40')/2),
-        width: wp('40') , height: wp('40')
+        bottom: hp('-15'),
+        left: wp('50')-(hp('20')/2),
+        width: hp('20') , height: hp('20')
     },
 
     maincolor: {
         backgroundColor: "#FFFFFF"
 
     },
-
-    profilepic: {
-      position: "relative",
-      bottom: wp('-2.5'),
-      left: wp('4')
-    },
     title: {
     textAlign: 'center',
       position: "relative",
       color:'black',
       marginTop: wp('12')/4,
-      fontSize: 22,
+      fontSize: hp(2.4),
     fontWeight: 'bold' },
 
     title2: {
@@ -77,39 +71,39 @@ const styles = StyleSheet.create({
           position: "relative",
           color:'white',
           marginTop: wp('12')/4,
-          fontSize: 22,
+          fontSize: hp(2.4),
         fontWeight: 'bold' },
 
     textbetween:{
         textAlign: 'center',
         position: "relative",
         color:'black',
-        fontSize: 16,
-        bottom: wp('-45')},
+        fontSize: hp(1.9),
+        bottom: hp('-20')},
   
       
       buttonright: {
       position: "relative",
-      bottom: wp('-65'),
+      bottom: hp('-30'),
       left: wp('50')-(wp('40')/2),
-        marginBottom: 150 }, 
+        marginBottom: hp(20) }, 
 
       buttonleft: {
         position: "relative",
-        bottom: wp('-65'),
+        bottom: hp('-30'),
         left: wp('50')-(wp('55')/2)},
 
       tinput:{
         position: "relative",
-      bottom: wp('-60'),
+      bottom: hp('-27'),
       left: wp('50')-(wp('70')/2), 
       borderColor: 'black',
       borderWidth: 0.5,
       width: wp('70'), 
       height: wp('11'),
       borderRadius: 13,
-      marginBottom: 20,
-      paddingHorizontal: 7,
+      marginBottom: hp(1),
+      paddingHorizontal: wp(3),
 
       }
   
