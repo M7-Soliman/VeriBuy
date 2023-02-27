0686baee8a8feebf28ba1d3c44b77d4dadacdcc4
aiiitsh <ahesham368@gmail.com>
2023-02-27 22:08:51 +0200

    trying the gif style in the login page

--- screens/SigninScreen.js
+++ screens/SigninScreen.js
@@ -30,11 +30,14 @@ const SigninScreen = () => {
       })
     }
     return (
-
       <ScrollView style = {styles.maincolor}>
         <View>
 
+            <Image source={require("../assets/giphy.gif")} style = {styles.photo} className ="rounded-3xl"/>
             <Image source={require("../assets/LogoGraybg.jpeg")} style = {styles.vbuylogo} className ="rounded-3xl"/>
+            <View style={{width: wp('100') , height: wp('10'), backgroundColor:"#26da76"}} className="rounded-3xl"/>
+                
+           
 
             <TextInput style={styles.tinput} placeholder="Email" keyboardType="Default"
             onChangeText={(text) => setEmail(text)}/>
@@ -55,7 +58,7 @@ const SigninScreen = () => {
             }
 
             <TouchableOpacity onPressIn={()=>{navigation.navigate("SignupScreen")}} onPress={()=> SetSuccess(0)} style = {styles.buttonleft} className="flex-row">
-            <View style={{width: wp('55') , height: wp('12'), backgroundColor:"#bd31fe"}} className="rounded-3xl">
+            <View style={{width: wp('40') , height: wp('12'), backgroundColor:"#bd31fe"}} className="rounded-3xl">
                 <Text style={ styles.title2} className="Bold">Sign Up</Text>
             </View>
             </TouchableOpacity>
@@ -71,16 +74,25 @@ const SigninScreen = () => {
 
 const styles = StyleSheet.create({
 
-    vbuylogo:{
-        bottom: hp('-15'),
-        left: wp('50')-(hp('20')/2),
-        width: hp('20') , height: hp('20')
+    photo:{
+        bottom: hp('-7'),
+        left: wp('0')-(hp('20')/2),
+        width: hp('100') , height: hp('25')
     },
+    vbuylogo:{
+      bottom: hp('10'),
+      left: wp('45')-(hp('20')/2),
+      width: hp('25') , height: hp('25'), 
+      shadowColor: '#171717',
+    shadowOffset: {width: -2, height: 4},
+    shadowOpacity: 0.2,
+    shadowRadius: 3
+  },
 
     redtext :{
       position: 'absolute',
       color: 'red',
-      bottom: hp('20'),
+      bottom: hp('80'),
       left: wp(50) - 105,
     },
 
@@ -107,25 +119,25 @@ const styles = StyleSheet.create({
     textbetween:{
         textAlign: 'center',
         position: "relative",
-        color:'black',
-        fontSize: hp(1.9),
-        bottom: hp('-20')},
+        color:'blue',
+        fontSize: hp(2.5),
+        bottom: hp('50')},
   
       
       buttonright: {
       position: "relative",
-      bottom: hp('-30'),
+      bottom: hp('2'),
       left: wp('50')-(wp('40')/2),
-        marginBottom: hp(20) }, 
+        marginBottom: hp(62) }, 
 
       buttonleft: {
         position: "relative",
-        bottom: hp('-30'),
-        left: wp('50')-(wp('55')/2)},
+        bottom: hp('60'),
+        left: wp('50')-(wp('40')/2)},
 
       tinput:{
         position: "relative",
-      bottom: hp('-27'),
+      bottom: hp('5'),
       left: wp('50')-(wp('70')/2), 
       borderColor: 'black',
       borderWidth: 0.5,
