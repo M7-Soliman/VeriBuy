a43e45d1d69a5560e1c3f209f69ba470ac8cad6b
aiiitsh <ahesham368@gmail.com>
2023-02-28 15:54:39 +0200

    login page design updated
    I am so happy with the result

--- screens/SigninScreen.js
+++ screens/SigninScreen.js
@@ -4,7 +4,6 @@ import { useLayoutEffect, useState} from 'react'
 import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
 import { useDispatch } from 'react-redux';
 import { login } from '../redux/actions';
-
 const SigninScreen = () => {
     const navigation = useNavigation();
     useLayoutEffect(() => {
@@ -32,37 +31,45 @@ const SigninScreen = () => {
     return (
       <ScrollView style = {styles.maincolor}>
         <View>
+        
 
             <Image source={require("../assets/giphy.gif")} style = {styles.photo} className ="rounded-3xl"/>
+            
+            <View style={styles.mainbox} className="rounded-3xl">
+            <Text style={ styles.toptext} >WELCOME! </Text>
+            <Text style={ styles.toptext2} >Hi there! How are you? it's been </Text>
+            <Text style={ styles.downtext} >Not registered yet? </Text>
             <Image source={require("../assets/LogoGraybg.jpeg")} style = {styles.vbuylogo} className ="rounded-3xl"/>
-            <View style={{width: wp('100') , height: wp('10'), backgroundColor:"#26da76"}} className="rounded-3xl"/>
-                
-           
-
-            <TextInput style={styles.tinput} placeholder="Email" keyboardType="Default"
+            <TextInput style={styles.tinput} placeholderTextColor='white' underlineColorAndroid='white'placeholder="Email" keyboardType="Default"
             onChangeText={(text) => setEmail(text)}/>
-            <TextInput style={styles.tinput} placeholder="Password" keyboardType="Default"
+            <TextInput style={styles.tinput} placeholderTextColor='white' underlineColorAndroid='white' placeholder="Password" keyboardType="Default"
             onChangeText={(text) => setPassword(text)} secureTextEntry={true}/>
 
 
             <TouchableOpacity onPressIn={()=> handleLogin()} style = {styles.buttonright}  className="flex-row">
             <View style={{width: wp('40') , height: wp('12'), backgroundColor:"#26da76"}} className="rounded-3xl">
-                <Text style={ styles.title} className="Bold">Sign In</Text>
+                <Text style={ styles.title} className="Bold">SIGN IN</Text>
             </View>
             </TouchableOpacity >
 
             {Success == 0?
                 <Text style={ styles.redtext}> </Text>
                 :
-                <Text style={ styles.redtext} > Check your email and password </Text>
+                <Text style={ styles.redtext} >Wrong email or password </Text>
             }
 
             <TouchableOpacity onPressIn={()=>{navigation.navigate("SignupScreen")}} onPress={()=> SetSuccess(0)} style = {styles.buttonleft} className="flex-row">
-            <View style={{width: wp('40') , height: wp('12'), backgroundColor:"#bd31fe"}} className="rounded-3xl">
-                <Text style={ styles.title2} className="Bold">Sign Up</Text>
+            <View style={{width: wp('20') , height: wp('8'), backgroundColor:"#bd31fe"}} className="rounded-xl">
+                <Text style={ styles.title2} className="Bold">SIGN UP</Text>
             </View>
             </TouchableOpacity>
-                <Text style={ styles.textbetween} className="Bold">Not Registered Yet? </Text>
+            
+                
+
+            </View>
+                
+           
+
 
  
             
@@ -75,25 +82,66 @@ const SigninScreen = () => {
 const styles = StyleSheet.create({
 
     photo:{
-        bottom: hp('-7'),
+        bottom: hp('-5'),
         left: wp('0')-(hp('20')/2),
         width: hp('100') , height: hp('25')
     },
     vbuylogo:{
-      bottom: hp('10'),
+      bottom: hp('25'),
       left: wp('45')-(hp('20')/2),
       width: hp('25') , height: hp('25'), 
-      shadowColor: '#171717',
+      shadowColor: 'white',
     shadowOffset: {width: -2, height: 4},
     shadowOpacity: 0.2,
     shadowRadius: 3
   },
+  toptext:{
+        
+    position: "relative",
+    color:'white',
+    fontSize: hp(4.8),
+    bottom: hp('-14'),
+    left: wp('25')-(wp('40')/2),
+    fontFamily: "Roboto", 
+    fontWeight: "bold"
+    
+  },
+  toptext2:{
+        
+    position: "relative",
+    color:'white',
+    fontSize: hp(2),
+    bottom: hp('-14'),
+    left: wp('25')-(wp('40')/2),
+    fontFamily: "Roboto", 
+    fontWeight: "normal"
+    
+  },
+  downtext:{
+        
+    position: "relative",
+    color:'white',
+    fontSize: hp(2),
+    bottom: hp('-55'),
+    left: wp('27')-(wp('40')/2),
+    fontFamily: "Roboto", 
+    fontWeight: "normal"
+    
+  },
+  mainbox:{
+    width: wp('100') ,
+     height: hp('82'), 
+     backgroundColor:"black",
+    //  bottom: hp('21')
+    
+   
+},
 
     redtext :{
       position: 'absolute',
       color: 'red',
-      bottom: hp('80'),
-      left: wp(50) - 105,
+      bottom: hp('36'),
+      left: wp(31) - 105,
     },
 
     maincolor: {
@@ -103,49 +151,53 @@ const styles = StyleSheet.create({
     title: {
     textAlign: 'center',
       position: "relative",
-      color:'black',
+      color:'white',
       marginTop: wp('12')/4,
       fontSize: hp(2.4),
-    fontWeight: 'bold' },
+    fontWeight: 'bold', 
+    fontFamily: "Roboto", },
 
     title2: {
         textAlign: 'center',
           position: "relative",
           color:'white',
-          marginTop: wp('12')/4,
-          fontSize: hp(2.4),
-        fontWeight: 'bold' },
+          marginTop: wp('6')/4,
+          fontSize: hp(2),
+        fontWeight: 'bold', 
+        fontFamily: "Roboto", 
+       },
 
-    textbetween:{
-        textAlign: 'center',
-        position: "relative",
-        color:'blue',
-        fontSize: hp(2.5),
-        bottom: hp('50')},
+    
   
       
       buttonright: {
       position: "relative",
       bottom: hp('2'),
-      left: wp('50')-(wp('40')/2),
+      left: wp('25')-(wp('40')/2),
         marginBottom: hp(62) }, 
 
       buttonleft: {
         position: "relative",
-        bottom: hp('60'),
-        left: wp('50')-(wp('40')/2)},
+        bottom: hp('50'),
+        left: wp('26')-(wp('40')/2)
+      },
 
       tinput:{
         position: "relative",
-      bottom: hp('5'),
-      left: wp('50')-(wp('70')/2), 
-      borderColor: 'black',
+        
+      bottom: hp('6'),
+      left: wp('39')-(wp('70')/2), 
+      
       borderWidth: 0.5,
       width: wp('70'), 
-      height: wp('11'),
+      height: wp('10'),
       borderRadius: 13,
       marginBottom: hp(1),
-      paddingHorizontal: wp(3),
+      paddingHorizontal: wp(3.5),
+      color: "white",
+      
+      fontFamily: "Roboto", 
+    fontWeight: "normal"
 
       }
   
