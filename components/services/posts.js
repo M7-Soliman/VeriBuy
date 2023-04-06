7df8cc13ac817cb9ad67299e25a09b554ecb8186
Mohamed (Soliman) Elhussiny <mohamed.s.elhussiny@gmail.com>
2023-04-06 19:01:22 +0200

    report action

--- components/services/posts.js
+++ components/services/posts.js
@@ -31,3 +31,17 @@ export const buyAction = (uid, name, address, number, postId, postmedia, seller,
     creation: firebase.firestore.FieldValue.serverTimestamp(),
   })
 }
+
+export const reportAction = (uid, reason, postId, postmedia, seller) =>{
+  firebase
+  .firestore()
+  .collection("orders")
+  .doc(postId)
+  .collection(uid)
+  .add({
+    uid, 
+    postmedia,
+    seller,
+    creation: firebase.firestore.FieldValue.serverTimestamp(),
+  })
+}
