193d659d8dc76b7af4f6cdf531acd8d493bcaaa2
Mohamed (Soliman) Elhussiny <mohamed.s.elhussiny@gmail.com>
2023-05-06 00:15:08 +0300

    Icona & fix camera issue

--- screens/camera/styles.js
+++ screens/camera/styles.js
@@ -2,7 +2,7 @@ import { StyleSheet } from "react-native";
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        marginTop: 45         
+        // marginTop: 45         
     },
     camera: {
         flex: 1,
